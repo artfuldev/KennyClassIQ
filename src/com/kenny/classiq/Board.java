0d0a98e7bd3a534efd51caef261a8fbe97f95d01
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-25 13:40:09 +0530

    First Commit
    Version 0.0
    Duh!

---
+++ src/com/kenny/classiq/Board.java
@@ -0,0 +1,13 @@
+package com.kenny.classiq;
+
+/**
+ * The <code>Board</code> class is the class which represents a chess board.
+ * It is made up of objects of the class <code>Square</code>, which are
+ * also members of objects of classes <code>Rank</code>, <code>File</code>,
+ * and <code>Diagonal</code>.
+ * @author Kenshin Himura (Sudarsan Balaji)
+ */
+public class Board
+{
+
+}
