b341f3caeab904e450a1bbda7a4bcc5a54f84a40
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-26 12:48:00 +0530

    Comments Update

--- src/com/kenny/classiq/UCI.java
+++ src/com/kenny/classiq/UCI.java
@@ -1,5 +1,14 @@
 package com.kenny.classiq;
 
+/**
+ * <code>UCI</code> is the class used to represent the Universal Chess
+ * Interface(UCI) communication protocol. It implements the
+ * <code>CommunicationProtocol</code> interface to make use of its
+ * functions, which are common to both <code>XBoard</code> and
+ * <code>UCI</code> protocols.
+ * @author Kenshin Himura
+ *
+ */
 public class UCI implements CommunicationProtocol
 {
 	public void init()
@@ -7,8 +16,13 @@ public class UCI implements CommunicationProtocol
 		System.out.println("id name Kenny ClassIQ 0.0.0.1");
 		System.out.println("id author Kenshin Himura (Sudarsan Balaji)");
 		System.out.println("uciok");
+		listen();
 	}
-	public void sendDetails()
+	public void listen()
+	{
+		
+	}
+	public void execute(String cmd)
 	{
 		
 	}
