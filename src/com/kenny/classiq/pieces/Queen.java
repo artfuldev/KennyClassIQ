1ec4228ec26a7c64b11b46e59b31eed20fe32c67
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-29 15:39:22 +0530

    Queen Update

---
+++ src/com/kenny/classiq/pieces/Queen.java
@@ -0,0 +1,12 @@
+package com.kenny.classiq.pieces;
+
+import com.kenny.classiq.definitions.PieceValues;
+
+public class Queen extends Piece
+{
+	public Queen()
+	{
+		pieceValue=PieceValues.queenValue;
+		shortAlgebraicNotation="Q";
+	}
+}
