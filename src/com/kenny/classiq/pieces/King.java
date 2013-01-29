1ba6fcf1a8a7662d00544f5ad457183c45b11e7c
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-29 15:39:27 +0530

    King Update

---
+++ src/com/kenny/classiq/pieces/King.java
@@ -0,0 +1,12 @@
+package com.kenny.classiq.pieces;
+
+import com.kenny.classiq.definitions.PieceValues;
+
+public class King extends Piece
+{
+	public King()
+	{
+		pieceValue=PieceValues.kingValue;
+		shortAlgebraicNotation="K";
+	}
+}
