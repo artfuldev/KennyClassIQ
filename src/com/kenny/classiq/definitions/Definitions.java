c09270a6688abc28f39fe9d26503f65dcfb75fd3
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-29 15:37:59 +0530

    Reorganization Update

---
+++ src/com/kenny/classiq/definitions/Definitions.java
@@ -0,0 +1,37 @@
+package com.kenny.classiq.definitions;
+
+/**
+ * Includes the standard definitions needed.
+ * I previously used to work with C++. Since Java lacks the <code>#define</code>
+ * tags used to define constants in C++, I found this workaround. Making
+ * the members static makes them accessible without using an object, and making
+ * them final makes them constant, just the functionality offered by the 
+ * <code>#define</code> tags. I have to include the class name, yes, but its
+ * better than not having such a functionality at all!
+ * @author Kenshin Himura (Sudarsan Balaji)
+ *
+ */
+public class Definitions
+{
+	/**
+	 * Holds the author name of the program.
+	 */
+	public static final String authorName="Kenshin Himura (Sudarsan Balaji)";
+	/**
+	 * Holds the name of the engine.
+	 */
+	public static final String engineName="Kenny ClassIQ";
+	/**
+	 * Holds the version of the engine.
+	 */
+	public static final String engineVersion="0.0.0.3";
+	/**
+	 * Holds the error string.
+	 */
+	public static final String errorString="error";
+	/**
+	 * Holds the string to output an error in the recieved command,
+	 * usually one which is not understood by the engine.
+	 */
+	public static final String errorInCommandString="Error (unknown command): ";
+}
