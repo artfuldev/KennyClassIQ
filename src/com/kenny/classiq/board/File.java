1dc9b6aa107ddc6cee121f607bb126b0cef3ab8c
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-30 12:20:03 +0530

    File Update

--- src/com/kenny/classiq/board/File.java
+++ src/com/kenny/classiq/board/File.java
@@ -2,5 +2,34 @@ package com.kenny.classiq.board;
 
 public class File
 {
-
+	private Square[] square;
+	private Board board;
+	private String name;
+	public Square getSquare(String squareName)
+	{
+		for(int i=0;i<square.length;i++)
+			if(square[i].getName().matches(squareName))
+				return square[i];
+		return null;
+	}
+	public void setSquare(Square square, byte index)
+	{
+		this.square[index] = square;
+	}
+	public Board getBoard()
+	{
+		return board;
+	}
+	public void setBoard(Board board)
+	{
+		this.board = board;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 }
