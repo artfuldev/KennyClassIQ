053abc06f510d69a05dbc38c295e6ac779f18417
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-31 00:58:23 +0530

    File Construction Update

--- src/com/kenny/classiq/board/File.java
+++ src/com/kenny/classiq/board/File.java
@@ -5,6 +5,11 @@ public class File
 	private Square[] square;
 	private Board board;
 	private String name;
+	public File(String name)
+	{
+		setName(name);
+		square=new Square[8];
+	}
 	public Square getSquare(String squareName)
 	{
 		for(int i=0;i<square.length;i++)
