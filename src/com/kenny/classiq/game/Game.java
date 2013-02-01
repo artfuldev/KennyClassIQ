30d05a502c8186ea5598ace5c597cfdc220c282a
Kenshin Himura <kenshin.thebattosai@live.com>
2013-02-01 11:45:04 +0530

    newGame() Update
    fixes #45

--- src/com/kenny/classiq/game/Game.java
+++ src/com/kenny/classiq/game/Game.java
@@ -15,10 +15,38 @@ public class Game
 	private PieceSet pieceSet;
 	public Game()
 	{
-		gameBoard=new Board();
+		gameBoard=new Board(this);
 		lastMovedPiece=null;
 		playerOne=new Player(this,"white","gui");
 		playerTwo=new Player(this,"black","ai");
 		currentPlayer=playerTwo;
+		pieceSet=new PieceSet();
+	}
+	public void newGame()
+	{
+		for(byte i=8;i<16;i++)
+			gameBoard.setPiece(pieceSet.getPiece("white","pawn"),i);
+		for(byte i=48;i<56;i++)
+			gameBoard.setPiece(pieceSet.getPiece("black","pawn"),i);
+		gameBoard.setPiece(pieceSet.getPiece("white","rook"),"a1");
+		gameBoard.setPiece(pieceSet.getPiece("white","rook"),"h1");
+		gameBoard.setPiece(pieceSet.getPiece("black","rook"),"a8");
+		gameBoard.setPiece(pieceSet.getPiece("black","rook"),"h8");
+		gameBoard.setPiece(pieceSet.getPiece("white","knight"),"b1");
+		gameBoard.setPiece(pieceSet.getPiece("white","knight"),"g1");
+		gameBoard.setPiece(pieceSet.getPiece("black","knight"),"b8");
+		gameBoard.setPiece(pieceSet.getPiece("black","knight"),"g8");
+		gameBoard.setPiece(pieceSet.getPiece("white","bishop"),"c1");
+		gameBoard.setPiece(pieceSet.getPiece("white","bishop"),"f1");
+		gameBoard.setPiece(pieceSet.getPiece("black","bishop"),"c8");
+		gameBoard.setPiece(pieceSet.getPiece("black","bishop"),"f8");
+		gameBoard.setPiece(pieceSet.getPiece("white","queen"),"d1");
+		gameBoard.setPiece(pieceSet.getPiece("white","king"),"e1");
+		gameBoard.setPiece(pieceSet.getPiece("black","queen"),"d8");
+		gameBoard.setPiece(pieceSet.getPiece("black","king"),"e8");
+	}
+	public void showBoard()
+	{
+		gameBoard.printBoard();
 	}
 }
