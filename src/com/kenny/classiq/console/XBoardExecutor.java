283f12730e897ac8b6907a7080c90e74294f5058
Kenshin Himura <kenshin.thebattosai@live.com>
2013-02-03 00:51:24 +0530

    Updated Comments

--- src/com/kenny/classiq/console/XBoardExecutor.java
+++ src/com/kenny/classiq/console/XBoardExecutor.java
@@ -1,7 +1,25 @@
 package com.kenny.classiq.console;
 
+/**
+ * The <code>XBoardExecutor</code> class extends <code>Executor</code> and
+ * thus inherits its run() method by default. All that remains is to define
+ * its execute() method properly, according to the <code>XBoard</code>
+ * protocol.
+ * @author Kenshin Himura (Sudarsan Balaji)
+ *
+ */
 public class XBoardExecutor extends Executor
 {
+	/**
+	 * It is the function which executes the commands parsed by the
+	 * listener. It is used to check for specific cases and then call
+	 * the respective functions to execute such commands, by passing
+	 * the correct corresponding paramaters to those functions, according
+	 * to the <code>XBoard</code> protocol. It is yet to be defined
+	 * fully.
+	 * @param commandString The full command to be executed, as a
+	 * <code>String</code>.
+	 */
 	public void execute(String commandString)
 	{
 		String[] splitString=commandString.split("\\s");
