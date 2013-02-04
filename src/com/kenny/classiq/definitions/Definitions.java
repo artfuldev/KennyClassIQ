254ebc94ea3916bb6d62f407bcec33ae75aeb90c
Kenshin Himura <kenshin.thebattosai@live.com>
2013-02-04 14:47:39 +0530

    Version 0.0.10.2 Update

--- src/com/kenny/classiq/definitions/Definitions.java
+++ src/com/kenny/classiq/definitions/Definitions.java
@@ -24,7 +24,7 @@ public class Definitions
 	/**
 	 * Holds the version of the engine.
 	 */
-	public static final String engineVersion="0.0.10.1 build 876";
+	public static final String engineVersion="0.0.10.2 build 892";
 	/**
 	 * Holds the error string.
 	 */
