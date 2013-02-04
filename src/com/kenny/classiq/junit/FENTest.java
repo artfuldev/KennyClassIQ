b3b7406625a1ccd9b836ec7f9830162a7fa5fe28
Kenshin Himura <kenshin.thebattosai@live.com>
2013-02-05 02:08:57 +0530

    Added jUnit FENTest

---
+++ src/com/kenny/classiq/junit/FENTest.java
@@ -0,0 +1,28 @@
+package com.kenny.classiq.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.kenny.classiq.definitions.Definitions;
+import com.kenny.classiq.game.Game;
+
+public class FENTest
+{
+	@Test
+	public void test()
+	{
+		Game chessGame=new Game(Definitions.startPositionFEN);
+		chessGame.showBoard();
+		String newFen="rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/"
+				+"PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+		Game chessGame2=new Game(newFen);
+		chessGame2.showBoard();
+		newFen="rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
+		Game chessGame3=new Game(newFen);
+		chessGame3.showBoard();
+		assertEquals("Great!","c6",chessGame3.getEnPassantSquare().getName());
+		assertEquals("Nice!",0,chessGame3.getHalfMoveClock(),0);
+		assertEquals("Wow!",2,chessGame3.getMoveNumber(),0);
+	}
+}
