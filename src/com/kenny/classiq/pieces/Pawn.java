0f9e2d7b6a4c9e73430850c285cde9d2b617619d
Kenshin Himura <kenshin.thebattosai@live.com>
2013-02-10 18:48:08 +0530

    Added getMoves() proper definition for Pawn

--- src/com/kenny/classiq/pieces/Pawn.java
+++ src/com/kenny/classiq/pieces/Pawn.java
@@ -17,6 +17,52 @@ public class Pawn extends Piece
 	{
 		ArrayList<Move> returnList=new ArrayList<Move>();
 		ArrayList<Square> toSquares=new ArrayList<Square>();
-		return returnList;
+		if(square!=null)
+		{
+			if(white)
+			{
+				if(square.getTopLeftSquare()!=null)
+					if(square.getTopLeftSquare().getPiece()!=null)
+						if(square.getTopLeftSquare().getPiece().isWhite()!=white)
+							toSquares.add(square.getTopLeftSquare());
+				if(square.getTopRightSquare()!=null)
+					if(square.getTopRightSquare().getPiece()!=null)
+						if(square.getTopRightSquare().getPiece().isWhite()!=white)
+							toSquares.add(square.getTopRightSquare());
+				if(square.getTopSquare()!=null)
+					if(square.getTopSquare().getPiece()==null)
+						toSquares.add(square.getTopSquare());
+				if(Byte.parseByte(square.getRank().getName())==2)
+					if(square.getTopSquare()!=null)
+						if(square.getTopSquare().getTopSquare()!=null)
+							if(square.getTopSquare().getTopSquare().getPiece()==null)
+								toSquares.add(square.getTopSquare().getTopSquare());
+			}
+			else
+			{
+				if(square.getBottomLeftSquare()!=null)
+					if(square.getBottomLeftSquare().getPiece()!=null)
+						if(square.getBottomLeftSquare().getPiece().isWhite()!=white)
+							toSquares.add(square.getBottomLeftSquare());
+				if(square.getBottomRightSquare()!=null)
+					if(square.getBottomRightSquare().getPiece()!=null)
+						if(square.getBottomRightSquare().getPiece().isWhite()!=white)
+							toSquares.add(square.getBottomRightSquare());
+				if(square.getBottomSquare()!=null)
+					if(square.getBottomSquare().getPiece()==null)
+						toSquares.add(square.getBottomSquare());
+				if(Byte.parseByte(square.getRank().getName())==7)
+					if(square.getBottomSquare()!=null)
+						if(square.getBottomSquare().getBottomSquare()!=null)
+							if(square.getBottomSquare().getBottomSquare().getPiece()==null)
+								toSquares.add(square.getBottomSquare().getBottomSquare());
+			}
+			if(!toSquares.isEmpty())
+				for(byte i=0;i<toSquares.size();i++)
+					returnList.add(new Move(square,toSquares.get(i)));
+			if(!returnList.isEmpty())
+				return returnList;
+		}
+		return null;
 	}
 }
