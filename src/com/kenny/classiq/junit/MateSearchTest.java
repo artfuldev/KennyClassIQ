2220eb0cd9a74ac3a3e4990fc692395156b1d119
Kenshin Himura <kenshin.thebattosai@live.com>
2013-02-19 01:06:35 +0530

    Added a jUnit Test Case to check if mate-search works

---
+++ src/com/kenny/classiq/junit/MateSearchTest.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of "Kenny ClassIQ", (c) Kenshin Himura, 2013.
+ * 
+ * "Kenny ClassIQ" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * "Kenny ClassIQ" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with "Kenny ClassIQ".  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package com.kenny.classiq.junit;
+
+import org.junit.Test;
+import com.kenny.classiq.game.Game;
+
+public class MateSearchTest
+{
+	@Test
+	public void test()
+	{
+		String FEN="rnbqkbnr/pppp1ppp/4p3/8/5PP1/8/PPPPP2P/RNBQKBNR b KQkq g3 0 2 ";
+		Game chessGame=new Game(FEN);
+		chessGame.printStats();
+		chessGame.getPlayerTwo().getMove();
+		chessGame.printStats();
+	}
+}
